7343e9d91af5ec1b5e95be3204c083fa946fcdd1
Adam Beziou <adam.beziou@gmail.com>
2018-06-06 22:45:28 -0600

    "Parking" in Python

--- Python/Parking.py
+++ Python/Parking.py
@@ -1,13 +1,32 @@
-cases = int(input())
+totalPrice = 0
+timeline = []
 
-for i in range(cases):
-	dist = 0
-	num = int(input())
-	spots = input().split()
-	spots = [int(x) for x in spots]
-	spots.sort()
-		
-	for i in range(num - 1):
-		dist += spots[i+1] - spots[i]
-		
-	print(dist * 2)
+prices = input().split()
+prices = [int(x) for x in prices]
+
+firstTruckTimes = input().split()
+firstTruckTimes = [int(x) for x in firstTruckTimes]
+secondTruckTimes = input().split()
+secondTruckTimes = [int(x) for x in secondTruckTimes]
+thirdTruckTimes = input().split()
+thirdTruckTimes = [int(x) for x in thirdTruckTimes]
+
+totalTimeList = firstTruckTimes + secondTruckTimes + thirdTruckTimes
+totalTimeList.sort()
+
+for i in range(totalTimeList[len(totalTimeList) - 1]):
+	timeline.append(0)
+
+for i in range(firstTruckTimes[0], firstTruckTimes[1]):
+	timeline[i] += 1
+
+for i in range(secondTruckTimes[0], secondTruckTimes[1]):
+	timeline[i] += 1
+
+for i in range(thirdTruckTimes[0], thirdTruckTimes[1]):
+	timeline[i] += 1
+
+for i in range(len(timeline)):
+	totalPrice += prices[timeline[i] - 1] * timeline[i]
+
+print(totalPrice)
